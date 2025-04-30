06885d6b9d7c1a9dcac97f6a3288e04f945b38c8
Zac Smith <48374908+ZacDSmith@users.noreply.github.com>
2025-04-29 21:21:45 -0400

    Custom Creative Tab with Sapphire and Raw Sapphire

---
+++ src/main/java/net/zac/tutorialmod/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.zac.tutorialmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.zac.tutorialmod.TutorialMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    // Initializes the item. This is what we do to create custom items.
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
+            () -> new Item(new Item.Properties()));
+
+    // Registers the item
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
