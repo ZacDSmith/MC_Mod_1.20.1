1a04c3f3741a4d62b1c4ba5f65820936e7e387bb
Zac Smith <48374908+ZacDSmith@users.noreply.github.com>
2025-05-01 00:08:20 -0400

    Added Sapphire ore, Deepslate Sapphire ore, Nether Sapphire ore, and Endstone Sapphire ore.
    Added Metal detector that detects Iron ore, Diamond ore, Deepslate Iron Ore, and Deepslate Diamond ore.

--- src/main/java/net/zac/tutorialmod/item/ModItems.java
+++ src/main/java/net/zac/tutorialmod/item/ModItems.java
@@ -6,6 +6,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.zac.tutorialmod.TutorialMod;
+import net.zac.tutorialmod.item.custom.MetalDetectorItem;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -16,6 +17,10 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
             () -> new Item(new Item.Properties()));
+
+    // Registeres metal dectector
+    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
+            () -> new MetalDetectorItem(new Item.Properties().durability(100)));
 
     // Registers the item
     public static void register(IEventBus eventBus){
