c2e8ea1d6a4b4d95725f174fe00824774579816e
Zac Smith <48374908+ZacDSmith@users.noreply.github.com>
2025-05-04 17:15:05 -0400

    Data gen stage 2, Json files now generated instead of hand made.

--- src/main/java/net/zac/tutorialmod/datagen/loot/ModBlockLootTables.java
+++ src/main/java/net/zac/tutorialmod/datagen/loot/ModBlockLootTables.java
@@ -19,7 +19,7 @@ import net.zac.tutorialmod.item.ModItems;
 import java.util.Set;
 
 public class ModBlockLootTables extends BlockLootSubProvider{
-    protected ModBlockLootTables() {
+    public ModBlockLootTables() {
         super(Set.of(), FeatureFlags.REGISTRY.allFlags());
     }
 
@@ -27,7 +27,7 @@ public class ModBlockLootTables extends BlockLootSubProvider{
     protected void generate() {
         this.dropSelf(ModBlocks.SAPPHIRE_BLOCK.get());
         this.dropSelf(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
-        //this.dropSelf(ModBlocks.SOUND_BLOCK.get());
+        this.dropSelf(ModBlocks.SOUND_BLOCK.get());
 
         this.add(ModBlocks.SAPPHIRE_ORE.get(),
                 block -> createOreDrops(ModBlocks.SAPPHIRE_ORE.get(), ModItems.RAW_SAPPHIRE.get()));
